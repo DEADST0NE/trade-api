391731ccbfe82af2f6090af2c38087955e44ba14
Ruslan <deadstone@list.ru>
2021-02-03 17:43:08 +0300

    test

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
 const start = async() => { 
-  console.log("Server ready at: http://localhost:", PORT, "\n");
+  console.log("Server test ready at: http://localhost:", PORT, "\n");
   app.listen(PORT); 
 }
 
