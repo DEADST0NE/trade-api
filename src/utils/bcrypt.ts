9e4bdb0b83ac9f5d22a7e7d6385dbe5df7fda5fa
Ruslan Pahlivanov <deadstone@list.ru>
2021-02-12 15:28:51 +0300

    deploy

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -3,4 +3,4 @@ import bcrypt from 'bcrypt';
 const compare = (password: string, hash: string) => bcrypt.compare(password, hash);
 const hash = (password: string) => bcrypt.hash(password, 10);
 
-export { compare, hash };
+export { compare, hash }; 
